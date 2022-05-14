b42b6da18485946fa4aba3966fb0a46415738826
Ariel Triana <usich37@gmail.com>
2022-05-13 20:51:45 -0400

    🆕 Add Home page scheme
    Add:
    - Navbar
    - GetStarted btn
    - Main img

--- roar-ui/src/app/app.module.ts
+++ roar-ui/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+
+import { MatButtonModule } from "@angular/material/button";
+import {  MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
